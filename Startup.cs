0c26b11e936fb04376f503f47475d1ae036a3a0e
Nelly <nellysattari@gmail.com>
2017-07-27 11:36:35 +1000

    mvc proj

---
+++ Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_speedup
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            //we do configurations/env var
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)//fallback- we will override the previous
+            // .AddJsonFile($"appsettings.localdev.json", optional: true)//you can git ignore it and only use for each dev env
+                //multiple configuration files can be added
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            //dependency injecton
+
+            // services.AddTransient();//new once I request
+            // services.AddScoped();//keep it alive per framework lifecycle - good place for DBcontext injection
+            // services.AddSingleton();//new once keep alive
+            
+            // this is TagHelperServicesExtensions first task consideration so no worries for adding injection
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();//for serving images-css
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });//bring whole MVC engine
+        }
+    }
+}
